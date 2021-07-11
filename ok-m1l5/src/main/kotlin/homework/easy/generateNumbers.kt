000cf560ad7cd49a67a12296f6b2f1dd4d6cf875
alexxxov1 <73472823+alexxxov1@users.noreply.github.com>
2021-07-11 14:34:41 +0300

    courotine easy task done

--- ok-m1l5/src/main/kotlin/homework/easy/generateNumbers.kt
+++ ok-m1l5/src/main/kotlin/homework/easy/generateNumbers.kt
@@ -1,5 +1,5 @@
 package homework.easy
 
-fun generateNumbers() = (0..10000).map {
+suspend fun generateNumbers() = (0..10000).map {
     (0..100).random()
 }
--- ok-m1l5/src/main/kotlin/homework/easy/main.kt
+++ ok-m1l5/src/main/kotlin/homework/easy/main.kt
@@ -1,20 +1,34 @@
 package homework.easy
 
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
+
 fun main() {
-    val numbers = generateNumbers()
-    val toFind = 10
-    val toFindOther = 1000
-
-    val foundNumbers = listOf(
-        findNumberInList(toFind, numbers),
-        findNumberInList(toFindOther, numbers)
-    )
-
-     foundNumbers.forEach {
-         if (it != -1) {
-             println("Your number found!")
-         } else {
-             println("Not found number $toFind")
-         }
-     }
+    val time = measureTimeMillis {
+        runBlocking {
+            val numbers = coroutineScope{async {generateNumbers()}}.await()
+            val foundNumbers = coroutineScope {
+                (0..10)
+                    .map {
+                        async {
+                            findNumberInList(it, numbers)
+                        }
+                    }.awaitAll()
+            }
+//            val num1 = async {  findNumberInList(10, numbers) }
+//            val num2 = async {  findNumberInList(1000, numbers) }
+//            val foundNumbers = listOf(
+//                num1.await(), num2.await()
+//            )
+            println(foundNumbers)
+            foundNumbers.forEach {
+                if (it != -1) {
+                    println("Your number found $it")
+                } else {
+                    println("Not found number $it")
+                }
+            }
+        }
+    }
+    println("Time: $time")
 }
